6595bba4edd95344f79ad9a8c5e9493f1746ecba
unknown <IvyLeano@outlook.com>
2019-10-23 15:21:59 -0400

    buttons added in MainActivity

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView calculatorScreen = findViewById(R.id.calculatorScreen);
+
+        final Button n0 = findViewById(R.id.n0);
+        final Button n1 = findViewById(R.id.n1);
+        final Button n2 = findViewById(R.id.n2);
+        final Button n3 = findViewById(R.id.n3);
+        final Button n4 = findViewById(R.id.n4);
+        final Button n5 = findViewById(R.id.n5);
+        final Button n6 = findViewById(R.id.n6);
+        final Button n7 = findViewById(R.id.n7);
+        final Button n8 = findViewById(R.id.n8);
+        final Button n9 = findViewById(R.id.n9);
+        final Button dot = findViewById(R.id.dot);
+        final Button equals = findViewById(R.id.equals);
+        final Button addition = findViewById(R.id.addition);
+        final Button subtraction = findViewById(R.id.subtraction);
+        final Button multiplication = findViewById(R.id.multiplication);
+        final Button division = findViewById(R.id.division);
     }
 }
