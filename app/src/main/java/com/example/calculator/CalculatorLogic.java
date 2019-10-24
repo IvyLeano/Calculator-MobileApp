ba15e8dfb0e32cf4601b6b12261345ce75aaf781
unknown <IvyLeano@outlook.com>
2019-10-24 15:07:27 -0400

    added 'equal' calculator logic

--- app/src/main/java/com/example/calculator/CalculatorLogic.java
+++ app/src/main/java/com/example/calculator/CalculatorLogic.java
@@ -1,11 +1,17 @@
 package com.example.calculator;
 
 // This class contains all the logic for a basic calculator application
-// No B.E.D.M.A.S.S functionality included, calculations are done  
+// No B.E.D.M.A.S.S functionality included, calculations are done
 public class CalculatorLogic {
     Double total = 0.00;  //total calculated value
 
     public String equal(String mathEquation) {
+        String[] splitMathEquation = mathEquation.split("\\s+");
+        total = Double.valueOf(splitMathEquation[0]);
+
+        for(int i = 1; i < splitMathEquation.length; i+=2){
+
+        }
         return "equal button hit";
     }
     public void addition() {
